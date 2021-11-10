d6b9c935072a54f20e36c19ef58f3ae68cc05e2e
adacameron <ada.e.cameron@gmail.com>
2021-11-10 20:51:18 +0000

    GetFlight component (empty)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/App.css";
-import GetFlight from "../requests/getToken";
+import GetFlight from "./GetFlight";
 
 function App() {
   return (
---
+++ src/components/GetFlight.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GetFlight = () => {
+  return (
+    <div>
+      <p>text</p>
+    </div>
+  );
+};
+
+export default GetFlight;
