0f266060982050d2512775fa3b7528f3d8e8ac95
adacameron <ada.e.cameron@gmail.com>
2021-12-26 17:03:40 +0000

    Positioning icons & styling form date input

--- frontend/src/requests/getInspLocation.js
+++ frontend/src/requests/getInspLocation.js
@@ -2,18 +2,15 @@
 import axios from "axios";
 
 const getInspLocation = (destinations) => {
-  console.log(destinations, "destinations");
   let endpoint = "http://localhost:1338/location/";
 
   destinations.forEach((destination) => {
     endpoint += `${destination}`;
   });
-  console.log(endpoint, "endpoint");
   return axios
 
     .get(endpoint)
     .then((res) => {
-      console.log("res.data[0].name", res.data);
       return res.data;
     })
     .catch((err) => {
